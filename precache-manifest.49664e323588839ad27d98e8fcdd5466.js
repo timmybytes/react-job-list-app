e410e675345285cd66294fefa71bce8d59520634
Timothy Merritt <timmybytes@gmail.com>
2020-08-17 07:25:51 -0700

    Updates

---
+++ precache-manifest.49664e323588839ad27d98e8fcdd5466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90de67ca9c8cd536fc8d72544623f41",
+    "url": "/react-job-list-app/index.html"
+  },
+  {
+    "revision": "a68da4b07aef795a3c19",
+    "url": "/react-job-list-app/static/css/main.b044d04a.chunk.css"
+  },
+  {
+    "revision": "5bef7f0907ff68f5736d",
+    "url": "/react-job-list-app/static/js/2.b6b60e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-job-list-app/static/js/2.b6b60e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68da4b07aef795a3c19",
+    "url": "/react-job-list-app/static/js/main.080282e9.chunk.js"
+  },
+  {
+    "revision": "f678b2d6ae250071e0c8",
+    "url": "/react-job-list-app/static/js/runtime-main.b6591b3b.js"
+  }
+]);
